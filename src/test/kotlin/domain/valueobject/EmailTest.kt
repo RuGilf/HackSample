fa109e9c19333835ebe047b882f2187a9e8e03a1
RuGilf <hackjackbrain@gmail.com>
2026-08-14 18:00:29 +0300

    add email test

--- src/test/kotlin/domain/valueobject/EmailTest.kt
+++ src/test/kotlin/domain/valueobject/EmailTest.kt
@@ -1,4 +1,24 @@
 package domain.valueobject
 
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
 class EmailTest {
+    @Test
+    fun `valid email is created`() {
+        val email = Email("antihype@hype.train")
+
+        assertEquals(
+            expected = "antihype@hype.train",
+            actual = email.value
+        )
+    }
+
+    @Test
+    fun `email without at sign is rejected`() {
+        assertFailsWith<IllegalArgumentException> {
+            Email("dot.dot.dot")
+        }
+    }
 }
